3fcad7da73facf2b183b9b7fdd21241d8d36b3d7
hanyoung <phanyoung@naver.com>
2021-12-15 12:36:17 +0900

    2021-12-15 수 스택 수정

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import numbers
+
 class Stack:
     def __init__(self):
       self.items = []
@@ -6,45 +8,93 @@ class Stack:
       self.items.append(item)
     
     def pop(self):
-      try:
         return self.items.pop()
-      except IndexError:
-        print('Stack is empty')
 
     def top(self):
-      try:
         return self.items[-1]
-      except IndexError:
-        print('Stack is empty')
-        return False
+
+    def is_empty(self):
+        return bool(len(self.items) == 0)
 
     def __len__(self):
-      return len(self.items)
+        return len(self.items)
 
-# infix > postfix > 연산
-# 피연산자: top stack
-# +-*/: opstack / (): opstack / 
-def to_postfix(formula):
-    topstack = []
+def split_tokens(tokens):
+    result = []
+    val = 0
+    val_processing = False
+    for i in tokens:
+        if i in '0123456789':
+            i = int(i)
+            val = val * 10 + i
+            val_processing = True
+        else:
+            if val_processing:
+                result.append(val)
+                val = 0
+                val_processing = False
+            result.append(i)
+
+    if val_processing:
+        result.append(val)
+    return result
+
+def into_postfix(token_list):
+    result = []
     opstack = Stack()
-    for i in formula:
-        if i.isnumeric():
-            topstack.append(i)
-        elif i == '+' or i ==  '-':
-            if opstack.top() == '*' or opstack.top() == '/':
-                topstack.append(opstack.pop())
-            opstack.push(i)
-        elif i == '(':
-            opstack.push(i)
-        elif i == '*' or i == '/':
-            opstack.push(i)
-        elif i == ')':
-            while True:
-                a = opstack.pop()
-                if a == '(':
-                    break
-                topstack.append(a)
-    while opstack.top():
-        topstack.append(opstack.pop())
-    return topstack
+    priority = {
+      '+': 1,
+      '-': 1,
+      '(': 1,
+      '*': 2,
+      '/': 2,
+      ')': 3
+    }
 
+    for i in token_list:
+        if isinstance(i, int):
+            result.append(i)
+        elif i == ')':
+            while not opstack.top() == '(':
+                result.append(opstack.pop())
+            opstack.pop()
+        else:
+            if opstack.is_empty():
+                opstack.push(i)
+            else:
+              if priority[i] < priority[opstack.top()]:
+                  result.append(opstack.pop())
+                  opstack.push(i)
+              else:
+                  opstack.push(i)
+    if not opstack.is_empty():
+        while not opstack.is_empty():
+            result.append(opstack.pop())
+    return result
+
+def calculate(postfix):
+    int_stack = Stack()
+    for i in postfix:
+        if isinstance(i, numbers.Number):
+            int_stack.push(i)
+        else:
+            n1 = int_stack.pop()
+            n2 = int_stack.pop()
+            if i == '+':
+                new_int = n1 + n2
+            if i == '-':
+                new_int = n1 - n2
+            if i == '*':
+                new_int = n1 * n2
+            if i == '/':
+                new_int = n2 / n1
+            int_stack.push(new_int)
+    return int_stack.top()
+
+def cal_tokens(tokens):
+    token_list = split_tokens(tokens)
+    postfix = into_postfix(token_list)
+    result = calculate(postfix)
+    return result
+
+print(cal_tokens('(3+2)*3/136'))
